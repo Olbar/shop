c7cb1995b4934b9389e66a9b7f150804fc178453
Olbar <Olegb1984@mail.ru>
2019-07-17 08:24:22 +0300

    добавление товара в корзину

---
+++ target/classes/static/js/shop/add_to_basket.js
@@ -0,0 +1,27 @@
+// jQuery(document) - $(document)
+// $(document).ready(function() {});
+
+$(document).ready(() => {
+
+    // $('#send-button').on('click', function() {});
+    $('#add_basket').on('click', sendActionToServer);
+
+});
+
+function sendActionToServer() {
+    // item id
+  const itemData = {
+        itemId: $('#item').attr('item-id') // get attribute value
+    };
+
+    $.ajax({
+        url: '/items/' + itemData.itemId + '/basket',  // /items/1/basket
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(itemData)
+
+    })
+            .done(() => {console.log('success')})
+            .fail(() => { console.log('fail'); });
+    });
+}
